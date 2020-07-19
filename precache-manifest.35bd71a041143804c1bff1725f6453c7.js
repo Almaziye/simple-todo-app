7faf82ba6e0c7dc9e15078a1c85e67e8a3c14274
Fitsum1 Feleke <fitsumfeleke7@gmail.com>
2020-07-19 13:58:37 -0400

    Updates

---
+++ precache-manifest.35bd71a041143804c1bff1725f6453c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6bd5cb1401e742406538cead745757",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "2e81663db4faed480909",
+    "url": "/simple-todo-app/static/css/main.0ebaf745.chunk.css"
+  },
+  {
+    "revision": "ed8b4a3a03320a25670b",
+    "url": "/simple-todo-app/static/js/2.b23e1c4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.b23e1c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e81663db4faed480909",
+    "url": "/simple-todo-app/static/js/main.6b9722ad.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
